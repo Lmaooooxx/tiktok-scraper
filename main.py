70829f24e7851ff7a09d02e72facdea79009fe44
Lmaooooxx <dungmanh24082006@gmail.com>
2025-11-16 20:32:40 +0700

    update scraper

--- main.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from tiktok_tool.tool_impl import TikTokToolImpl
-
-async def main():
-    tool = TikTokToolImpl(cookies_file="cookies.json")
-    await tool.run("channels.txt", "videos_today.json")
-
-if __name__ == "__main__":
-    asyncio.run(main())
